7542728e6ed34908cc2a8144d95a5a39d8b2930c
Zainab Yasser <202302304@stu.uob.edu.bh>
2026-04-08 23:04:16 +0300

    Done

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
  */
 function changeHeadingText() {
   // TODO: Implement this function
+  const heading = document.getElementById("main-heading");
+  heading.textContent= "DOM Manipulation Challenge";
 }
 
 /**
@@ -10,6 +12,8 @@ function changeHeadingText() {
  */
 function changeBoxColor() {
   // TODO: Implement this function
+    const box = document.getElementById("box-to-modify");
+    box.style.backgroundColor = 'lightblue';
 }
 
 /**
@@ -17,6 +21,9 @@ function changeBoxColor() {
  */
 function addNewItem() {
   // TODO: Implement this function
+  const li = document.createElement ("li");
+  li.textContent = "New Item";
+  document.getElementById("item-list").append(li);
 }
 
 /**
@@ -24,6 +31,8 @@ function addNewItem() {
  */
 function highlightParagraph() {
   // TODO: Implement this function
+const paragraph = document.querySelector('.content-para');
+paragraph.classList.add('highlight');
 }
 
 /**
@@ -31,6 +40,8 @@ function highlightParagraph() {
  */
 function removeElement() {
   // TODO: Implement this function
+  const element = document.getElementById('to-be-removed');
+  element.remove();
 }
 
 
